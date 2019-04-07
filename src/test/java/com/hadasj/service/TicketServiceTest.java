08f4c74daca520a1448c7bf89817663ad843c724
Jan Hadaš <hadas.jan@gmail.com>
2019-04-07 04:54:21 +0200

    JUnit with Mockito

--- src/test/java/com/hadasj/service/TicketServiceTest.java
+++ src/test/java/com/hadasj/service/TicketServiceTest.java
@@ -1,40 +1,36 @@
 package com.hadasj.service;
 
-import com.hadasj.Application;
 import com.hadasj.dao.impl.TicketDaoImpl;
 import com.hadasj.entity.Ticket;
-import org.junit.Before;
+import com.hadasj.service.impl.TicketServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class)
+
+@RunWith(MockitoJUnitRunner.class)
 public class TicketServiceTest {
 
-    // TODO: mock dao
-    @Autowired
-    private TicketService service;
-
-    @Autowired
+    @Mock
     private TicketDaoImpl ticketDao;
 
-    @Before
-    public void setUp() {
-        ticketDao.init();
-    }
+    @InjectMocks
+    private TicketServiceImpl service;
 
     @Test
-    public void generateTest() {
-        generate(5);
+    public void generateWithEmptyQueueTest() {
+        when(ticketDao.next()).thenReturn(6L);
+
         Ticket result = service.create();
 
         assertNotNull(result);
@@ -43,39 +39,62 @@ public class TicketServiceTest {
         assertNotNull(result.getCreateTime());
         assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), result.getCreateTime().truncatedTo(ChronoUnit.SECONDS));
         assertNotNull(result.getOrder());
-        assertEquals(6, result.getOrder().longValue());
+        assertEquals(1, result.getOrder().longValue());
     }
 
     @Test
-    public void getTest() {
-        generate(4);
+    public void generateWithNonEmptyQueueTest() {
+        when(ticketDao.next()).thenReturn(7L);
+        Ticket last = new Ticket();
+        last.setOrder(1L);
+        when(ticketDao.getLast()).thenReturn(Optional.of(last));
 
-        Optional<Ticket> actual = service.getActual();
-        assertNotNull(actual);
-        assertTrue(actual.isPresent());
-        assertEquals(1, actual.get().getId().longValue());
-        assertNotNull(actual.get().getCreateTime());
-        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), actual.get().getCreateTime().truncatedTo(ChronoUnit.SECONDS));
-        assertNotNull(actual.get().getOrder());
-        assertEquals(1, actual.get().getOrder().longValue());
+        Ticket result = service.create();
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertEquals(7, result.getId().longValue());
+        assertNotNull(result.getCreateTime());
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), result.getCreateTime().truncatedTo(ChronoUnit.SECONDS));
+        assertNotNull(result.getOrder());
+        assertEquals(2, result.getOrder().longValue());
     }
 
     @Test
-    public void deleteTest() {
-        generate(1);
-
+    public void getFromEmptyQueueTest() {
         Optional<Ticket> actual = service.getActual();
-        assertNotNull(actual);
-        assertTrue(actual.isPresent());
 
-        service.deleteLast();
-        actual = service.getActual();
         assertNotNull(actual);
         assertFalse(actual.isPresent());
     }
 
-    private void generate(final int count) {
-        for (int i = 0; i < count; i++)
-            service.create();
+    @Test
+    public void getFromNonEmptyQueueTest() {
+        Ticket first = new Ticket();
+        first.setId(8L);
+        first.setCreateTime(LocalDateTime.now());
+        first.setOrder(3L);
+        when(ticketDao.getFirst()).thenReturn(Optional.of(first));
+
+        Optional<Ticket> actual = service.getActual();
+
+        assertNotNull(actual);
+        assertTrue(actual.isPresent());
+        assertEquals(8, actual.get().getId().longValue());
+        assertNotNull(actual.get().getCreateTime());
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), actual.get().getCreateTime().truncatedTo(ChronoUnit.SECONDS));
+        assertNotNull(actual.get().getOrder());
+        assertEquals(3, actual.get().getOrder().longValue());
+    }
+
+    @Test
+    public void deleteTest() {
+        Ticket last = new Ticket();
+        last.setId(5L);
+        when(ticketDao.getLast()).thenReturn(Optional.of(last));
+
+        service.deleteLast();
+
+        verify(ticketDao).delete(5);
     }
 }
